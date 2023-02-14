9bc59d291752e5fed570a71124e63eae30b28962
Marina Dianova <m_di@mail.ru>
2023-02-14 18:16:40 +0300

    Homework 6 update

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -13,25 +13,37 @@ void CrossPoint(double a1, double a2, double c1, double c2, out double x, out do
     y = a1 * x + c1;
 }
 
+bool Validation(double a1, double a2, double c1, double c2)
+{
+if (a1 == a2 && c1 != c2)
+    {System.Console.WriteLine("Прямые параллельны");
+    return false;
+    }
+
+else if (a1 == a2 && c1 == c2)
+    {System.Console.WriteLine("Прямые совпадают");
+    return false;
+    }
+
+else if (a1 == 0 && c1 == 0)
+    {System.Console.WriteLine("Первая прямая не существует");
+    return false;
+    }
+
+else if (a2 == 0 && c2 == 0)
+    {System.Console.WriteLine("Вторая прямая не существует");
+    return false;
+    }
+return true;
+}
+
 double b1 = GetNumber("Введите коэффициент B1: ");
 double k1 = GetNumber("Введите коэффициент K1: ");
 double b2 = GetNumber("Введите коэффициент B2: ");
 double k2 = GetNumber("Введите коэффициент K2: ");
 
-
-if (k1 == k2 && b1 != b2)
-    System.Console.WriteLine("Прямые параллельны");
-
-else if (k1 == k2 && b1 == b2)
-    System.Console.WriteLine("Прямые совпадают");
-
-else if (k1 == 0 && b1 == 0)
-    System.Console.WriteLine("Первая прямая не существует");
-
-else if (k2 == 0 && b2 == 0)
-    System.Console.WriteLine("Вторая прямая не существует");
-else
+if (Validation(b1, b2, k1, k2))
 {
-    CrossPoint(k1, k2, b1, b2, out double x, out double y);
+    CrossPoint(b1, b2, k1, k2, out double x, out double y);
     System.Console.WriteLine($"Две прямые заданные уравнениями y = k1*x + b1, y = k2*x + b2 с заданными коэффициентами пересекаются в точке ({x}; {y})");
 }
